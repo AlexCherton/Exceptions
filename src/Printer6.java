37d36a4487b9c591de35afbaebf6c8269c84fcb7
Alex <Alex_General2002@mail.ru>
2024-01-25 21:46:17 +0300

    addition4

--- src/Printer6.java
+++ src/Printer6.java
@@ -2,24 +2,21 @@ import java.util.Arrays;
 
 class Expr {
     public static Double expr(int a, int b) {
-
         Double res = 0d;
-
-        String s;
-
-        res = (double) a / b;
-
+        if (b==0){
+            System.out.println("It's not possible to evaluate the expression - intArray[8] / d as d = 0");
+        }else {
+            String s;
+            res = (double) a / b;
+            printSum(a, b);
+        }
         return res;
     }
 
     public static void printSum(int a, int b) {
-
         int res = 0;
-
         String s;
-
         res = a + b;
-
         System.out.println(res);
     }
 }
@@ -29,20 +26,13 @@ public class Printer6 {
         int b;
         if (args.length==0) {
             a = 12;
-            b = 5;
+            b = 0;
         }else {
             a = Integer.parseInt(args[0]);
             b = Integer.parseInt(args[1]);
         }
 
-        if (b==0){
-            System.out.println("It's not possible to evaluate the expression - intArray[8] / d as d = 0");
-        }else {
             Double res = Expr.expr(a, b);
             System.out.println(res);
         }
     }
-
-
-
-}
