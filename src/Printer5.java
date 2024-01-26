80862fe92d87e317434a5df2e25d518cae5f879b
Алексей Юрьевич Ч <Alex_General2002@mail.ru>
2024-01-26 20:55:25 +0300

    addition5

--- src/Printer5.java
+++ src/Printer5.java
@@ -3,36 +3,37 @@ import java.util.Arrays;
 public class Printer5 {
     public static void main(String[] args) {
         int[] intArray;
-        int d = 0;
+        int d;
         if (args.length == 0) {
             intArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        }else {
+            d = 0;
+        } else {
             intArray = Arrays.stream(args[0].split(" ")).mapToInt(Integer::parseInt).toArray();
             d = Integer.parseInt(args[1]);
         }
 
-        if (d==0){
-            System.out.println("It's not possible to evaluate the expression - intArray[8] / d as d = 0");
-        }else if (intArray.length<9){
-            System.out.println("It's not possible to evaluate the expression - intArray[8] / d as there is"+
-                    "no 8th element in the given array");
-        }else {
-        String res = expr(intArray, d);
+
+        Double res = Expr.expr(intArray, d);
         System.out.println(res);
+
     }
-    }
 
-    public static String expr(int[] intArray, int d) {
+    class Expr {
+        public static Double expr(int[] intArray, int d) {
 
-        Float res = 0f;
+            Double res = 0d;
 
-        String s;
+            if (d == 0) {
+                System.out.println("It's not possible to evaluate the expression - intArray[8] / d as d = 0");
+            } else if (intArray.length < 9) {
+                System.out.println("It's not possible to evaluate the expression - intArray[8] / d as there is" +
+                        "no 8th element in the given array");
+            } else {
+                res = (double) intArray[8] / d;
+                System.out.println("intArray[8] / d = " + intArray[8] + " / " + d + " = " + res);
+            }
+            return res;
 
-        res = (float)intArray[8]/d;
-
-        s = "intArray[8] / d = " + intArray[8] + " / " + d + " = " + res;
-
-        return s;
         }
-
+    }
 }
